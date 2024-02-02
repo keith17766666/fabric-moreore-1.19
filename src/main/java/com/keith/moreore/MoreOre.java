e9139da31a9a0e5898f72a6e93ccf3867d11a371
keith17766666 <leek23014@gmail.com>
2024-02-02 20:57:49 +0800

    Init

---
+++ src/main/java/com/keith/moreore/MoreOre.java
@@ -0,0 +1,15 @@
+package com.keith.moreore;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreOre implements ModInitializer {
+	public static final String MOD_ID = "moreore";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/com/keith/moreore/MoreOreClient.java
@@ -0,0 +1,10 @@
+package com.keith.moreore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreOreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
